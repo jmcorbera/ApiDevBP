beed037b0bc0850e2881d00f7ac7107fdb472040
Juan M. Corbera <jcorbera@agrofy.com>
2024-05-11 14:10:46 -0300

    Add UserBusiness implementation

---
+++ src/ApiDevBP.Business/Contract/IUserBusiness.cs
@@ -0,0 +1,15 @@
+﻿using ApiDevBP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDevBP.Business.Contract
+{
+    public interface IUserBusiness
+    {
+        Task<int> SaveUser();
+        Task<IEnumerable<UserModel>> DeleteUser();
+    }
+}
