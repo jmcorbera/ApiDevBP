2eaa6e1db14118d421246e3921cf6ffaa4e3ea6c
Juan M. Corbera <jcorbera@agrofy.com>
2024-05-11 14:54:10 -0300

    add Service project

---
+++ src/ApiDevBP.Service/Implementation/UserService.cs
@@ -0,0 +1,24 @@
+﻿using ApiDevBP.Model.InputDTO;
+using ApiDevBP.Model.OutputDTO;
+using ApiDevBP.Service.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDevBP.Service.Implementation
+{
+    public class UserService : IUserService
+    {
+        public Task<IEnumerable<UserModelOutputDTO>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveUser(UserModelInputDTO user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
