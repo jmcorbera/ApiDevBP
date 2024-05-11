ca49e51c1aa74ffc8193de7f0dbb5abbde2ea3d3
Juan M. Corbera <jcorbera@agrofy.com>
2024-05-11 16:57:48 -0300

    Add endpoints to CRUD

--- src/ApiDevBP/Controllers/UsersController.cs
+++ src/ApiDevBP/Controllers/UsersController.cs
@@ -7,10 +7,13 @@ using System.Reflection;
 
 namespace ApiDevBP.Controllers
 {
+    /// <summary>
+    /// User endpoints
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class UsersController : ControllerBase
-    {   
+    {
         private readonly ILogger<UsersController> _logger;
 
         private readonly IUserBusiness _userBusiness;
@@ -21,21 +24,146 @@ namespace ApiDevBP.Controllers
             _userBusiness = userBusiness;
         }
 
+        /// <summary>
+        /// Method used to add new user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST 
+        ///     {
+        ///        "name": "name",
+        ///        "lastname": "lastname"
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        /// <response code="200">When the response is Success</response>
+        /// <response code="400">When the method has Bad Request</response>
         [HttpPost]
-        public async Task<IActionResult> SaveUser(UserModelInputDTO user)
+        [Route("SaveUser")]
+        public async Task<IActionResult> SaveUser([FromBody] UserModelInputDTO user)
         {
-            return Ok(await _userBusiness.SaveUser(user));
+            try
+            {
+                return Ok(await _userBusiness.SaveUser(user));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.InnerException?.Message ?? ex.Message);
+                return BadRequest();
+            }
         }
 
+        /// <summary>
+        /// Method used to get all users
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">When the response is Success</response>
+        /// <response code="400">When the method has Bad Request </response>
+        /// <response code="404">Users not found</response>
         [HttpGet]
+        [Route("GetUsers")]
         public async Task<IActionResult> GetUsers()
         {
-            var users = await _userBusiness.GetUsers();
-            if (users != null)
+            try
             {
-                return Ok(users);
+                var users = await _userBusiness.GetUsers();
+                if (users != null)
+                {
+                    return Ok(users);
+                }
+                return NotFound();
             }
-            return NotFound();
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Method used to get user by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="200">When the response is Success</response>
+        /// <response code="400">When the method has Bad Request </response>
+        /// <response code="404">User not found</response>
+        [HttpGet]
+        [Route("GetUserById")]
+        public async Task<IActionResult> GetUserById([FromQuery] int id)
+        {
+            try
+            {
+                var user = await _userBusiness.GetUserById(id);
+                if (user == null)
+                    return NotFound();
+
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Method used to update user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT 
+        ///     {
+        ///        "id": 0,
+        ///        "name": "name",
+        ///        "lastname": "lastname"
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        /// <response code="200">When the response is Success</response>
+        /// <response code="400">When the method has Bad Request </response>
+        [HttpPut]
+        [Route("UpdateUser")]
+        public async Task<IActionResult> UpdateUser([FromBody] UserModelInputDTO user)
+        {
+            try
+            {
+                return Ok(await _userBusiness.UpdateUser(user));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.InnerException?.Message ?? ex.Message);
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Method used to delete user
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="200">When the response is Success</response>
+        /// <response code="400">When the method has Bad Request </response>
+        [HttpDelete]
+        [Route("DeleteUser")]
+        public async Task<IActionResult> DeleteUser([FromQuery] int id)
+        {
+            try
+            {
+                await _userBusiness.DeleteUser(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.InnerException?.Message ?? ex.Message);
+                return BadRequest();
+            }
         }
 
     }
