e06561d36c6919d1cc0a12dac3b076429278d465
Juan M. Corbera <jcorbera@agrofy.com>
2024-05-11 14:37:06 -0300

    update models

---
+++ src/ApiDevBP.Model/InputDTO/UserModelInputDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDevBP.Model.InputDTO
+{
+    public class UserModelInputDTO
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ src/ApiDevBP.Model/OutputDTO/UserModelOutputDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDevBP.Model.OutputDTO
+{
+    public class UserModelOutputDTO
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+    }
+}
