922fdca6594de94f1896622f51f76e6b87a91fa0
Juan M. Corbera <jcorbera@agrofy.com>
2024-05-11 15:45:58 -0300

    add mapper to service

---
+++ src/ApiDevBP.Service/Mapper/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿using ApiDevBP.DataAccess.Entities;
+using ApiDevBP.Model.InputDTO;
+using ApiDevBP.Model.OutputDTO;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDevBP.Service.Mapper
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserModelInputDTO, UserEntity>();
+            CreateMap<UserEntity, UserModelOutputDTO>();
+        }
+    }
+}
